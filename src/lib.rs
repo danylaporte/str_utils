64768f1b5e45fcaf3e2ab721c063dce264b1055e
Dany Laporte <dany_laporte@hotmail.com>
2019-03-17 21:08:08 -0400

    add StrUtilsExt trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,4 @@
 pub mod cmp;
+mod str_utils_ext;
+
+pub use self::str_utils_ext::StrUtilsExt;
---
+++ src/str_utils_ext.rs
@@ -0,0 +1,417 @@
+use super::cmp::EqExt;
+
+/// Trait for searching string with accent / case insensitive comparison.
+pub trait StrUtilsExt {
+    /// Returns true if the given pattern matches a suffix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Press Café".ends_with_ai("Cafe"));
+    /// ```
+    fn ends_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a suffix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent / case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Press Café".ends_with_ai_ci("cafe"));
+    /// ```
+    fn ends_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a suffix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Press Cafe".ends_with_ci("cafe"));
+    /// ```
+    fn ends_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns the byte index of the first character of this string slice that matches the pattern.
+    /// Returns None if the pattern doesn't match.
+    /// 
+    /// The comparison is accent insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// let s = "Löwe 老虎 Léopard";
+    ///
+    /// assert_eq!(s.find_ai("L"), Some(0));
+    /// assert_eq!(s.find_ai("é"), Some(4));
+    /// assert_eq!(s.find_ai("Leopard"), Some(13));
+    /// ```
+    fn find_ai(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized;
+
+    /// Returns the byte index of the first character of this string slice that matches the pattern.
+    /// Returns None if the pattern doesn't match.
+    /// 
+    /// The comparison is accent / case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// let s = "Löwe 老虎 Léopard";
+    ///
+    /// assert_eq!(s.find_ai_ci("l"), Some(0));
+    /// assert_eq!(s.find_ai_ci("É"), Some(4));
+    /// assert_eq!(s.find_ai_ci("leopard"), Some(13));
+    /// ```
+    fn find_ai_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized;
+
+    /// Returns the byte index of the first character of this string slice that matches the pattern.
+    /// Returns None if the pattern doesn't match.
+    /// 
+    /// The comparison is accent / case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// let s = "Löwe 老虎 Léopard";
+    ///
+    /// assert_eq!(s.find_ci("l"), Some(0));
+    /// assert_eq!(s.find_ci("E"), Some(4));
+    /// assert_eq!(s.find_ci("léopard"), Some(13));
+    /// ```
+    fn find_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a prefix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Café Arabica".starts_with_ai("Cafe"));
+    /// ```
+    fn starts_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a prefix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent / case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Café Arabica".starts_with_ai_ci("cafe"));
+    /// ```
+    fn starts_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a prefix of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Cafe Arabica".starts_with_ci("cafe"));
+    /// ```
+    fn starts_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized;
+
+    /// Returns true if the given pattern matches a sub-slice of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Café Arabica".contains_ai("Cafe"));
+    /// ```
+    #[inline]
+    fn contains_ai(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        self.find_ai(pat).is_some()
+    }
+
+    /// Returns true if the given pattern matches a sub-slice of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is accent / case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Café Arabica".contains_ai_ci("cafe"));
+    /// ```
+    #[inline]
+    fn contains_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        self.find_ai_ci(pat).is_some()
+    }
+
+    /// Returns true if the given pattern matches a sub-slice of this string slice.
+    /// Returns false if it does not.
+    /// 
+    /// The comparison is case insensitive.
+    /// 
+    /// # Example
+    /// ```
+    /// use str_utils::StrUtilsExt;
+    /// 
+    /// assert!("Café Arabica".contains_ci("café"));
+    /// ```
+    #[inline]
+    fn contains_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        self.find_ci(pat).is_some()
+    }
+}
+
+impl StrUtilsExt for &str {
+    #[inline]
+    fn ends_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn ends_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn ends_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ci)
+    }
+
+    #[inline]
+    fn find_ai(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn find_ai_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn find_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ci)
+    }
+
+    #[inline]
+    fn starts_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn starts_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn starts_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ci)
+    }
+}
+
+impl StrUtilsExt for &String {
+    #[inline]
+    fn ends_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn ends_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn ends_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        ends_with(self, pat, EqExt::eq_ci)
+    }
+
+    #[inline]
+    fn find_ai(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn find_ai_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn find_ci(self, pat: &str) -> Option<usize>
+    where
+        Self: Sized,
+    {
+        find_str(self, pat, EqExt::eq_ci)
+    }
+
+    #[inline]
+    fn starts_with_ai(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ai)
+    }
+
+    #[inline]
+    fn starts_with_ai_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ai_ci)
+    }
+
+    #[inline]
+    fn starts_with_ci(self, pat: &str) -> bool
+    where
+        Self: Sized,
+    {
+        starts_with(self, pat, EqExt::eq_ci)
+    }
+}
+
+fn find_str<F>(src: &str, pat: &str, f: F) -> Option<usize>
+where
+    F: Fn(char, char) -> bool,
+{
+    for (index, _) in src.char_indices() {
+        return match search(src[index..].chars(), pat.chars(), &f) {
+            SearchResult::Found => Some(index),
+            SearchResult::NotFoundContinue => continue,
+            SearchResult::NotFoundFinal => None,
+        };
+    }
+
+    None
+}
+
+#[derive(Eq, PartialEq)]
+enum SearchResult {
+    /// Found
+    Found,
+    /// Not found and cannot continue searching
+    NotFoundFinal,
+    /// Not found and can continue searching
+    NotFoundContinue,
+}
+
+#[inline]
+fn ends_with<F>(src: &str, pat: &str, f: F) -> bool
+where
+    F: Fn(char, char) -> bool,
+{
+    search(src.chars().rev(), pat.chars().rev(), f) == SearchResult::Found
+}
+
+#[inline]
+fn starts_with<F>(src: &str, pat: &str, f: F) -> bool
+where
+    F: Fn(char, char) -> bool,
+{
+    search(src.chars(), pat.chars(), f) == SearchResult::Found
+}
+
+fn search<F, S, P>(mut src: S, mut pat: P, f: F) -> SearchResult
+where
+    F: Fn(char, char) -> bool,
+    S: Iterator<Item = char>,
+    P: Iterator<Item = char>,
+{
+    loop {
+        return match (src.next(), pat.next()) {
+            (Some(s), Some(p)) => {
+                if f(s, p) {
+                    continue;
+                }
+                SearchResult::NotFoundContinue
+            }
+            (None, Some(_)) => SearchResult::NotFoundFinal,
+            (_, None) => SearchResult::Found,
+        };
+    }
+}
